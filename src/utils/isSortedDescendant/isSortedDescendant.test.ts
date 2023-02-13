12de795a0a9b5de5fca9a6a73f6cd48c16843756
Gizmo <gizmo.dev.dev@gmail.com>
2023-02-13 11:29:52 +0300

    modify test for isSortedDescendant

--- src/utils/isSortedDescendant/isSortedDescendant.test.ts
+++ src/utils/isSortedDescendant/isSortedDescendant.test.ts
@@ -1,24 +1,32 @@
-import {describe, it, expect} from "vitest";
+import {describe, it, expect, expectTypeOf} from "vitest";
 import {isSortedDescendant} from "./isSortedDescendant";
 
 describe.concurrent('isSortedDescendant tests', () => {
   it('expect true', () => {
     const array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-    expect(isSortedDescendant(array)).toBeTruthy();
+    const res = isSortedDescendant(array);
+    expect(res).toBeTruthy();
+    expectTypeOf(res).toEqualTypeOf<boolean>()
   })
 
   it('expect false', () => {
     const array = [10, 90, 8, 7, 6, 5, 4, 3, 2, 1];
-    expect(isSortedDescendant(array)).toBeFalsy();
+    const res = isSortedDescendant(array);
+    expect(res).toBeFalsy();
+    expectTypeOf(res).toEqualTypeOf<boolean>()
   })
 
   it('empty array', () => {
     const array: number[] = [];
-    expect(isSortedDescendant(array)).toBeTruthy();
+    const res = isSortedDescendant(array);
+    expect(res).toBeTruthy();
+    expectTypeOf(res).toEqualTypeOf<boolean>()
   })
 
   it('same number', () => {
     const array: number[] = [1, 1, 1, 1, 1, 1, 1,];
-    expect(isSortedDescendant(array)).toBeTruthy();
+    const res = isSortedDescendant(array);
+    expect(res).toBeTruthy();
+    expectTypeOf(res).toEqualTypeOf<boolean>()
   })
 })
--- src/utils/isSortedDescendant/isSortedDescendant.ts
+++ src/utils/isSortedDescendant/isSortedDescendant.ts
@@ -1,8 +1,8 @@
 /**
  * Функция проверяет, что массим отсортирован по неубыванию
- * @param array
+ * @param array - входной массив
  */
-export const isSortedDescendant = (array: number[]) => {
+export const isSortedDescendant = (array: number[]): boolean => {
   return array.every((item, index, a) => {
     if (index === 0) return true;
     return a[index - 1] >= item;
